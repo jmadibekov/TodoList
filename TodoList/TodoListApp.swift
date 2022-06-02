e008603f314bb033fa8b1a4f86436358f743893f
Joseph Madibekov <jmadibekov@fb.com>
2022-06-02 22:42:39 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Joseph Madibekov on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
